d4de95a8b76008f067439ff8f4ed4a2a1dfd5e87
Gary White <garywhite61@hotmail.com>
2023-02-22 12:16:45 +0100

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function numberToPower(number, power){
+    console.info(Math.log2(1024));
+    // Code here
+}
+
+console.log(numberToPower(4, 2)); // 16
+console.log(numberToPower(10, 4)); // 10000
+console.log(numberToPower(10, 0)); // 1
